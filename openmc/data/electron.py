fb779c8a94bc2e7fb72f4c86d3e3f8e57dbebff5
GuySten <guyste@post.bgu.ac.il>
2026-08-10 19:51:52 +0300

    black

--- openmc/data/electron.py
+++ openmc/data/electron.py
@@ -3,8 +3,9 @@ import h5py
 from .data import Tabulated1D
 from .ace import Table
 
+
 class IncidentElectron:
-        """Continuous-energy incident electron interaction data parsed from ACE."""
+    """Continuous-energy incident electron interaction data parsed from ACE."""
 
     def __init__(self, atomic_number):
         self.atomic_number = atomic_number
@@ -21,6 +22,7 @@ class IncidentElectron:
             ace = ace_table_or_filename
         else:
             from .ace import Library
+
             lib = Library(ace_table_or_filename)
             ace = lib.tables[0]
 
@@ -57,8 +59,7 @@ class IncidentElectron:
 
         return data
 
-
-    def export_to_hdf5(self, path_or_group, mode='w'):
+    def export_to_hdf5(self, path_or_group, mode="w"):
         """Write data instance contents out to an HDF5 group layer."""
         if isinstance(path_or_group, h5py.Group):
             group = path_or_group
@@ -67,22 +68,21 @@ class IncidentElectron:
             group = f.create_group(f"z{self.atomic_number}")
 
         # Metadata
-        group.attrs['atomic_number'] = self.atomic_number
+        group.attrs["atomic_number"] = self.atomic_number
 
         # Master energy grid
-        group.create_dataset('energy', data=self.energy_grid)
+        group.create_dataset("energy", data=self.energy_grid)
 
         # Total individual cross section datasets
-        xs_group = group.create_group('cross_sections')
-        xs_group.create_dataset('elastic', data=self.elastic_xs)
-        xs_group.create_dataset('bremsstrahlung', data=self.bremsstrahlung_xs)
-        xs_group.create_dataset('excitation', data=self.excitation_xs)
+        xs_group = group.create_group("cross_sections")
+        xs_group.create_dataset("elastic", data=self.elastic_xs)
+        xs_group.create_dataset("bremsstrahlung", data=self.bremsstrahlung_xs)
+        xs_group.create_dataset("excitation", data=self.excitation_xs)
 
         # Subshell nested breakdown
-        sub_group = xs_group.create_group('ionization')
+        sub_group = xs_group.create_group("ionization")
         for subshell_name, xs_array in self.ionization_xs.items():
             sub_group.create_dataset(subshell_name, data=xs_array)
-        
 
 
 class ElectronAngularDistribution:
@@ -98,11 +98,11 @@ class ElectronAngularDistribution:
     def from_ace(cls, ace):
         """Parses elastic scattering distribution blocks from an ACE table object.
 
-                                                                            Parameters
-                                                                            ----------
-                                                                            ace : openmc.data.ace.Table
-                                                                                The underlying ACE table object being parsed.
-                                                                            """
+        Parameters
+        ----------
+        ace : openmc.data.ace.Table
+            The underlying ACE table object being parsed.
+        """
         j_elastic = ace.jxs[1]  # JXS(2) corresponds to elastic scattering data
         if j_elastic == 0:
             return None
@@ -127,7 +127,9 @@ class ElectronAngularDistribution:
             # EPRDATA14 files use 32 equiprobable bin boundaries
             n_bins = 32
             total_tab_elements = n_tabular_energies * n_bins
-            instance.tabular_pdf = ace.xss[idx : idx + total_tab_elements].reshape(n_tabular_energies, n_bins)
+            instance.tabular_pdf = ace.xss[idx : idx + total_tab_elements].reshape(
+                n_tabular_energies, n_bins
+            )
             idx += total_tab_elements
 
         # 3. Extract High-Energy Legendre Moments (Goudsmit-Saunderson Data)
@@ -137,25 +139,25 @@ class ElectronAngularDistribution:
             n_high_energies = n_energy_grid - n_tabular_energies
             total_leg_elements = n_high_energies * instance.n_legendre_coefficients
 
-            instance.legendre_coefficients = ace.xss[idx : idx + total_leg_elements].reshape(n_high_energies, instance.n_legendre_coefficients)
+            instance.legendre_coefficients = ace.xss[
+                idx : idx + total_leg_elements
+            ].reshape(n_high_energies, instance.n_legendre_coefficients)
 
         data.angular_distribution = ElectronAngularDistribution.from_ace(ace)
         return instance
 
     def to_hdf5(self, group):
         """Write the angular distribution datasets into an OpenMC HDF5 group."""
-        group.attrs['n_legendre_coefficients'] = self.n_legendre_coefficients
+        group.attrs["n_legendre_coefficients"] = self.n_legendre_coefficients
 
         if self.tabular_energies is not None:
-            tab_group = group.create_group('tabular')
-            tab_group.create_dataset('energies', data=self.tabular_energies)
-            tab_group.create_dataset('pdf_bins', data=self.tabular_pdf)
+            tab_group = group.create_group("tabular")
+            tab_group.create_dataset("energies", data=self.tabular_energies)
+            tab_group.create_dataset("pdf_bins", data=self.tabular_pdf)
 
         if self.legendre_coefficients is not None:
-            leg_group = group.create_group('legendre')
-            leg_group.create_dataset('coefficients', data=self.legendre_coefficients)
+            leg_group = group.create_group("legendre")
+            leg_group.create_dataset("coefficients", data=self.legendre_coefficients)
         if self.angular_distribution:
-            ang_group = group.create_group('angular_distribution')
+            ang_group = group.create_group("angular_distribution")
             self.angular_distribution.to_hdf5(ang_group)
-
-            
